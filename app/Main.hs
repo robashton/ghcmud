3f8772619c24d8b0ade131ffff78624c26bec8be
Rob Ashton <robashton@codeofrob.com>
2015-12-30 11:55:44 +0000

    a "Game loop" of sorts

--- app/Main.hs
+++ app/Main.hs
@@ -23,13 +23,26 @@ startGame :: World -> IO()
 startGame world =
   case initGameState world of
     Left RoomDoesNotExist -> errorOut "The room you were meant to start in did not exist"
-    Right state -> startGameLoop state
+    Right state ->
+      print "Your game starts now" >> inputLoop state
 
 errorOut :: String -> IO()
 errorOut msg = print ("Arse, something went tits up, here is a msg about that: " ++ msg)
 
-startGameLoop :: GameState -> IO()
-startGameLoop state = print "Ok"
+handleEndReason :: String -> IO()
+handleEndReason = print
+
+handleTextInstruction :: GameState -> String -> Either String (String, GameState)
+handleTextInstruction _ _ = Left "Not implemented yet"
+
+inputLoop :: GameState -> IO()
+inputLoop state =
+  do
+    instruction <- getLine
+    case handleTextInstruction state instruction of
+      Left endReason -> handleEndReason endReason
+      Right (msg, newState) ->
+          print msg >> inputLoop newState
 
 initGameState :: World -> Either FailFeedback GameState
 initGameState world =
