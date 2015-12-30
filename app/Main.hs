61e5ae02ab43635493571b756752d39a40eb29fb
Rob Ashton <robashton@codeofrob.com>
2015-12-30 11:34:29 +0000

    Force resolver something modern and also this is how one starts a game loop and handles errors at top level

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,43 @@ module Main where
 
 import Loader
 import World
+import Session
+import Control.Applicative ((<*>), empty)
+import Control.Monad (liftM)
+
+data GameState = GameState {
+  gameWorld :: World,
+  gameSession :: Session
+  } deriving (Show)
 
 main :: IO ()
-main = do
-  world <- loadDir "gaia/world"
-  printThing world
+main =
+  do
+    loadResult <- loadDir "gaia/world"
+    case loadResult of
+      Left (WorldLoadFailure msg) -> errorOut msg
+      Right world -> startGame world
 
-printThing :: Either WorldLoadFailure World -> IO()
-printThing (Left (WorldLoadFailure msg)) = print msg
-printThing (Right world) = print (show world)
+startGame :: World -> IO()
+startGame world =
+  case initGameState world of
+    Left RoomDoesNotExist -> errorOut "The room you were meant to start in did not exist"
+    Right state -> startGameLoop state
+
+errorOut :: String -> IO()
+errorOut msg = print ("Arse, something went tits up, here is a msg about that: " ++ msg)
+
+startGameLoop :: GameState -> IO()
+startGameLoop state = print "Ok"
+
+initGameState :: World -> Either FailFeedback GameState
+initGameState world =
+  (GameState world) <$> sessionStart (Coordinate 0 0) createPlayer world
+
+createPlayer :: Player
+createPlayer =
+  Player {
+    playerHealth = 10,
+    playerLevel = 1,
+    playerExperience = 0
+  }
